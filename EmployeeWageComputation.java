f579b34cce02c66336b1d162a7c6797e51c049be
RajaGithub12 <rajabaitha0218@gmail.com>
2022-09-05 14:22:12 +0530

    Adding:Refctoring code with classmethod

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,46 @@
+class EmployeeWageComputation {
+
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+
+    public static int calculateEmpWageForCompany(String company, int empRate, int numOFDays, int maxHours) {
+
+        System.out.println("----- Welcome to Employee Wage Computation! -----");
+        int empHrs=0, empWagePerMonth = 0, totalWorkingdays=0, totalEmpHours = 0;
+
+        while(totalEmpHours <= maxHours && totalWorkingdays < numOFDays) {
+
+            totalWorkingdays ++;
+            int empType = (int)Math.floor(Math.random() * 10) % 3;
+
+            switch(empType) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee Doing Full Time:");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee Doing Part Time:");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent:");
+                    empHrs = 0;
+            }
+
+            totalEmpHours += empHrs;
+            int empWagePerDay = empHrs * empRate;
+            empWagePerMonth += empWagePerDay;
+            System.out.println("Emp Wage Per Day : "+empWagePerDay);
+        }
+
+        System.out.println("Emp Wage Per Month for Company: "+company+" is : "+empWagePerMonth);
+        return empWagePerMonth;
+    }
+
+    public static void main(String[] args) {
+        calculateEmpWageForCompany("Raja", 20, 2, 10);
+        calculateEmpWageForCompany("Devraj", 10, 4, 20);
+
+    }
+
+}
